f32ed38197bb639583ed0ecc7425f64d9ff5f227
Robert Sanders Jr <robert.sanders24@gmail.com>
2021-06-26 22:42:30 -0400

    worked on javascript

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+$(document).ready(function(){
 //display the day
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").text(todayDate);
+
+//save information to local storage
+$('.saveBtn').on('click', function() {
+    
+})
+
+//get information from local storage
+
+
+//time indicator based on color (past,present,future)
+function timeIndicator() {
+    var currentTime = moment().hour();
+
+    $('.time-block').each(function (){
+        var hourBlock = parseInt($(this).attr('id').split('block'[1]));
+
+        if (hourBlock < currentTime) {
+            $(this).addClass('past');
+            $(this).removeClass('present');
+            $(this).removeClass('future');
+        }
+        else if (hourBlock === currentTime) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+        }
+        else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
+}
+timeIndicator();
+
+});
